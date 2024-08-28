ee36736e8a24ad5d529bed04260f0c0e39b02206
Jerin Sam <Jerin.sam@alignedautomation.com>
2024-08-28 21:28:57 +0530

    updated docker document

--- install_and_config/2-docker-compose-configure/docker_compose.sh
+++ install_and_config/2-docker-compose-configure/docker_compose.sh
@@ -1,15 +1,21 @@
-###### Landoop Kafka Details #######
+###### Landoop Kafka container #######
 # url : https://hub.docker.com/r/landoop/fast-data-dev 
-# details of the environment mentioned in docker compose can be found in the link
+# details of the environment mentioned in docker compose yaml file can be found in above link
 
+###### Elasticsearch container #########
+# url : https://hub.docker.com/_/elasticsearch
+
+# Download Docker Desktop for Windows and run it
 # Execute the below command to pull and configure all the images defined in the docker-compose.yaml file
 # this needs to be executed in the same folder where docker-compose.yaml file exists
 
-docker compose up -d
+docker compose up -d # to start all the services mentioned in the docker compose yaml file
 
-# To stop Container Services 
+docker-compose start kafka-cluster # to start only 1 service from the docker compose yaml file
 
-docker-compose stop
+docker-compose stop # to stop all container services 
+
+docker-compose stop kafka-cluster # to stop one container services 
 
 #To connect to services in Docker, refer to the following ports. 
   
@@ -18,6 +24,7 @@ docker-compose stop
 # REST Proxy, Schema Registry, Kafka Connect - 8081-8083:8081-8083
 # JMX - 9581-9585:9581-9585
 # Kafka Broker - 9092:9092
+# Elasticsearch - 9200
  
 
 ##### open docker bash in CMD and map local dev folder to docker - use below command ######
